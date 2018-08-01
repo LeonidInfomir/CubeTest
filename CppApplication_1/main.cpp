46a6d791d5d5220c8a41f4816ffa8ca93a320189
lbolyukh <leonid.org2000@gmail.com>
2018-08-01 14:38:58 +0300

    cpp code

---
+++ CppApplication_1/main.cpp
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: leonid.bolyukh
+ *
+ * Created on August 1, 2018, 1:47 PM
+ */
+
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    
+   cout << "hello!!" << endl;
+    return 0;
+}
+
